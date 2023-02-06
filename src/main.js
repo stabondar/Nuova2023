fbf5dfc1e6ba8dc1e08643157f6d4d593aac1bda
Denis Sizemov <96919055+Denis-Sizemov@users.noreply.github.com>
2023-02-06 17:29:13 +0200

    new

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './styles/style.css';
 import './styles/slider.css';
-import { gsap } from "gsap"
 
+import { gsap } from "gsap"
 import ServiceBlock from './moduls/ServiceBlock'
 import Loader from './moduls/Loader';
 import Slider from './moduls/Slider'
@@ -9,7 +9,6 @@ import Text from './moduls/Text'
 
 
 const checkPages = () => {
-
     const service = new ServiceBlock()
     const slider = new Slider()
     const text = new Text()
--- src/moduls/Text.js
+++ src/moduls/Text.js
@@ -1,6 +1,6 @@
 import gsap from "gsap";
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { SplitText } from 'gsap/SplitText'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { SplitText } from 'gsap/SplitText';
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
