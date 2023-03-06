b2771f61f1115568ac5ddacd1c987d14365d2dea
Denis Sizemov <96919055+Denis-Sizemov@users.noreply.github.com>
2023-03-06 11:16:14 +0200

    webflow slider

--- src/moduls/WebflowPage.js
+++ src/moduls/WebflowPage.js
@@ -1,24 +1,24 @@
-import Swiper, { Pagination, Navigation, EffectFade } from 'swiper'  
+import Swiper, { Navigation, EffectFade, EffectCards } from 'swiper'  
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/effect-cards';
 
 export default class WebflowPage {
 
     constructor() {
       let navList = $('.transformative__item')
 
-        $(".swiper-wrapper").append(`<div class="swiper-pagination"></div>`);
         const swiperBrands = new Swiper('._wf-slider', {
-          modules: [Pagination, Navigation, EffectFade],
+          modules: [Navigation, EffectFade],
           keyboard: true,
           fadeEffect: { crossFade: true },
           virtualTranslate: true,
           effect: "fade",
           grabCursor: true,
-          speed: 800,
-        //   loop: true,
+          speed: 1000,
+          // loop: true,
           slidersPerView: 1,
           centeredSlides: false,
           breakpoints: {
@@ -39,13 +39,8 @@ export default class WebflowPage {
           },
     
           navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-          },
-    
-          pagination: {
-            el: ".swiper-pagination",
-            clickable: true
+            nextEl: ".slider-arrow.is--next",
+            prevEl: ".slider-arrow.is--prev"
           }
         });
 
@@ -64,5 +59,22 @@ export default class WebflowPage {
           let index = self.index()
           swiperBrands.slideTo(index)
         })
+
+
+        const swiper = new Swiper(".swiper.dev-process", {
+          modules: [Navigation, EffectCards],
+          effect: "cards",
+          cardsEffect: {
+            perSlideRotate: 0,
+          },
+
+          grabCursor: true,
+          direction: "vertical",
+
+          navigation: {
+            nextEl: ".slider-arrow.is--dev-process.is--next",
+            prevEl: ".slider-arrow.is--dev-process.is--prev",
+          }
+        });
     }
 }
