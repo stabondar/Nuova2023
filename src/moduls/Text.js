6eec84be632374e5b434692ad7b0827ba8d17e47
Denis Sizemov <96919055+Denis-Sizemov@users.noreply.github.com>
2023-04-03 13:22:37 +0300

    borders

--- src/moduls/Text.js
+++ src/moduls/Text.js
@@ -143,7 +143,7 @@ export default class Text
             ScrollTrigger.refresh()
 
             setTimeout(() => {
-                gsap.set($('.works__item-thumbnail img'), {autoAlpha: 1})
+                gsap.set($('img'), {autoAlpha: 1})
             }, 1500);
 
             const lineAnimationResize = new SplitTextResize(splitLine, this.lineAnimation)
